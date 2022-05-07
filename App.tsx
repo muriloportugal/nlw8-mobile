b38df41cbcd0654c6f8eac0382e894d757fdd6d2
Murilo Portugal <murilo.portugal@gmail.com>
2022-05-06 22:56:45 -0300

    Iniciado aula 4

--- App.tsx
+++ App.tsx
@@ -1,20 +1,62 @@
+import { useEffect } from 'react';
+import { View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts, Inter_500Medium, Inter_400Regular } from '@expo-google-fonts/inter';
+
+import { theme } from './src/theme';
+import { Widget } from './src/components/Widget';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  useEffect(()=>{
+    async function prepare() {
+      if(!fontsLoaded) {
+        //Segura a splash screen até que as fontes sejam carregadas
+        await SplashScreen.preventAutoHideAsync();
+        // await new Promise(resolve => setTimeout(resolve, 2000));
+      }
+    };
+    
+    prepare();
+  }, []);
+  
+  useEffect(()=>{
+    async function prepare() {
+      if(fontsLoaded) {
+        //Remove a splashscreen quando a fonte for carregda
+        await SplashScreen.hideAsync();
+      }
+    };
+    prepare();
+  }, [fontsLoaded]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View 
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+      }}
+    >
+      <Widget />
+      <StatusBar 
+        style="light"
+        backgroundColor='transparent'
+        translucent
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ src/components/Widget/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { ChatTeardropDots } from 'phosphor-react-native';
+import { theme } from '../../theme';
+
+export function Widget() {
+  return (
+    <>
+      {/* <TouchableOpacity>
+        <ChatTeardropDots 
+          size={24}
+          weight="bold"
+          color={theme.colors.text_on_brand_color}
+        />
+      </TouchableOpacity> */}
+    </>
+  );
+}
